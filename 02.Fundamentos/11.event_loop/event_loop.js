a8f82ad02ce4a1c9d0157e2a0e4ee88328d954d1
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-20 09:29:55 -0300

    Event Loop

---
+++ 02.Fundamentos/11.event_loop/event_loop.js
@@ -0,0 +1,17 @@
+function a() {
+    console.log("Executando a()")
+}
+
+function b() {
+    console.log("Executando b()")
+}
+
+function c() {
+    console.log("Executando c()")
+    a()
+    b()
+}
+
+b()
+c()
+a()
