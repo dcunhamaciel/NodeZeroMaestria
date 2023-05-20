f2357c90ca795e342daaa00eaefde22e48647d26
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-20 09:26:05 -0300

    Abstração de Input com Inquirer

---
+++ 02.Fundamentos/10.abstracao_input/index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+    {
+        name: 'p1',
+        message: 'Qual a primeira nota?'
+    },
+    {
+        name: 'p2',
+        message: 'Qual a segunda nota?'
+    }])
+.then((answers) => {    
+    const media = (parseInt(answers.p1) + parseInt(answers.p2)) / 2
+    console.log(`A média é ${media}`)
+})
+.catch((error) => console.log(error))
