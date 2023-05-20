4a72905b346c86fe96f035c481eaf3eb5dd57ce8
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-20 13:33:14 -0300

    Trabalhando com Diretórios

---
+++ 03.Core_Modules/14.trabalhando_com_diretorios/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+if (!fs.existsSync('./minhapasta')) {
+    console.log('Não existe!')
+    fs.mkdirSync('minhapasta')    
+}
+
+if (fs.existsSync('./minhapasta')) {
+    console.log('Existe!')
+}
