ffc13bd502c72cddb7e259e7d767d4b90988d0d4
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-21 11:11:10 -0300

    Criação de Conta

--- 05.Accounts/index.js
+++ 05.Accounts/index.js
@@ -22,7 +22,48 @@ function operation() {
         .then((answer) => {
             const action = answer.action
 
-            console.log(action)
+            if (action === 'Criar Conta') {
+                createAccount()
+            }
+        })
+        .catch((error) => console.log(error))
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer
+        .prompt([
+            {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta:'
+            }
+        ])
+        .then((answer) => {
+            const accountName = answer.accountName
+            
+            console.log(accountName)
+
+            if (!fs.existsSync('accounts')) {
+                fs.mkdirSync('accounts')
+            }
+
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
+                buildAccount()
+                return
+            }
+
+            fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', (error) => console.log(error))
+
+            console.log(chalk.bgGreen('Parabéns, a sua conta foi criada!'))
+
+            operation()
         })
         .catch((error) => console.log(error))
 }
