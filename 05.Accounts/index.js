6c73d02aa7b7d1fae3f2b4d73bec839ce04f1639
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-21 10:56:46 -0300

    Inicialização do Sistema

--- 05.Accounts/index.js
+++ 05.Accounts/index.js
@@ -3,4 +3,26 @@ const chalk = require('chalk')
 
 const fs = require('fs')
 
-console.log('Iniciamos o Accounts')
+operation()
+
+function operation() {
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar',
+                'Sair'
+            ]
+        }])
+        .then((answer) => {
+            const action = answer.action
+
+            console.log(action)
+        })
+        .catch((error) => console.log(error))
+}
