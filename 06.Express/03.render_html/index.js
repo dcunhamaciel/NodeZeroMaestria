016c18b258e2242bb5d7f1eee7c99edcc1726eb8
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-23 20:06:58 -0300

    Renderização de HTML com Express

---
+++ 06.Express/03.render_html/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/', (request, response) => {
+    response.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
