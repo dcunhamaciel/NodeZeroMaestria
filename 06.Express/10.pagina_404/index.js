8bc8c8839c6951179ca8ce84df2723c6f6819c98
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-24 20:20:09 -0300

    Página 404

---
+++ 06.Express/10.pagina_404/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+const basePath = path.join(__dirname, 'templates')
+
+const usersRoute = require('./users')
+
+app.use(express.static('public'))
+
+app.use(
+    express.urlencoded({
+        extended: true
+    }))
+
+app.use(express.json())
+
+app.use('/users', usersRoute)
+
+app.get('/', (request, response) => {
+    response.sendFile(`${basePath}/index.html`)
+})
+
+app.use((request, response, next) => {
+    response
+        .status(404)
+        .sendFile(`${basePath}/pagina404.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
