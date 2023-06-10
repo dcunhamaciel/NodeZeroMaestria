c2847e345020a0c0412c8dc9528f25cffcd21f1c
dcunhamaciel <dcunhamaciel@gmail.com>
2023-06-10 09:10:10 -0300

    Excluindo registro relacionado

---
+++ 09.Sequelize/10.relacionamento_excluir/models/Address.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/conn')
+
+const User = require('./User')
+
+const Address = db.define('Address', {
+    street: {
+        type: DataTypes.STRING,
+        require: true
+    },
+    number: {
+        type: DataTypes.STRING,        
+        require: true
+    },
+    city: {
+        type: DataTypes.STRING,
+        require: true
+    }
+})
+
+User.hasMany(Address)
+Address.belongsTo(User)
+
+module.exports = Address
