e7d3020f38961e4f8856e3f711b976c54a41c4d6
dcunhamaciel <dcunhamaciel@gmail.com>
2023-06-17 15:36:49 -0300

    Finalizando Autenticação

--- 11.Thoughts/controllers/AuthController.js
+++ 11.Thoughts/controllers/AuthController.js
@@ -9,6 +9,36 @@ class AuthController {
         response.render('auth/login')
     }
 
+    static async loginPost(request, response) {
+        const { email, password } = request.body
+
+        const user = await User.findOne({ where: { email: email }})
+
+        if (!user) {
+            request.flash('message', 'Usuário não encontrado!')
+            response.render('auth/login')
+
+            return
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if (!passwordMatch) {
+            request.flash('message', 'Senha inválida!')
+            response.render('auth/login')
+
+            return
+        }
+
+        request.flash('message', 'Autenticação realizada com sucesso!')
+
+        request.session.userId = user.id
+
+        request.session.save(() => {
+            response.redirect('/')
+        })
+    }
+
     static logout(request, response) {
         request.session.destroy()
         
--- 11.Thoughts/routes/authRoutes.js
+++ 11.Thoughts/routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 
 router.get('/login', AuthController.login)
+router.post('/login', AuthController.loginPost)
 router.get('/logout', AuthController.logout)
 router.get('/register', AuthController.register)
 router.post('/register', AuthController.registerPost)
