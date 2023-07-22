daef8dc87d44baf123dbcbaaa72b07ae51dc96a5
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-22 09:58:03 -0300

    Validações de Usuário

--- 15.Get_a_Pet/backend/controllers/UserController.js
+++ 15.Get_a_Pet/backend/controllers/UserController.js
@@ -2,7 +2,46 @@ const User = require('../models/User')
 
 class UserController {
     static async register(request, response) {
-        response.json('Olá Get a Pet')
+        const { name, email, phone, password, confirmpassword } = request.body
+
+        if (!name) {
+            response.status(422).json({ message: 'O nome é obrigatório!' })
+            return
+        }
+
+        if (!email) {
+            response.status(422).json({ message: 'O e-mail é obrigatório!' })
+            return
+        }
+
+        if (!phone) {
+            response.status(422).json({ message: 'O telefone é obrigatório!' })
+            return
+        }
+
+        if (!password) {
+            response.status(422).json({ message: 'A senha é obrigatória!' })
+            return
+        }
+
+        if (!confirmpassword) {
+            response.status(422).json({ message: 'A confirmação de senha é obrigatória!' })
+            return
+        }
+
+        if (password !== confirmpassword) {
+            response.status(422).json({ message: 'As senhas não conferem!' })
+            return            
+        }
+
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists) {
+            response.status(422).json({ message: 'E-mail já cadastrado!' })
+            return
+        }
+
+        //
     }
 }
 
