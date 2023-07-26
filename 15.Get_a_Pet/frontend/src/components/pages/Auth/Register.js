955ed4b34e3997e940b742db5f050c228e77b84b
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-26 20:33:11 -0300

    Form de Registro

--- 15.Get_a_Pet/frontend/src/components/pages/Auth/Register.js
+++ 15.Get_a_Pet/frontend/src/components/pages/Auth/Register.js
@@ -1,8 +1,46 @@
+import Input from '../../form/Input'
+
 function Register() {
+    
+    function handleChange(e) {
+
+    }
+
     return (
         <section>
             <h1>Register</h1>
-        </section>
+            <form>
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite o seu telefone"
+                    handleOnChange={handleChange}
+                /> 
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Confirmação de senha"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirme a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Cadastrar" />           
+            </form>
+        </section>        
     )    
 }
 
