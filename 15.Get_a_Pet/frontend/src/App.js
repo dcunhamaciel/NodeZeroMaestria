1c758742614c9dd665ab0b8610616daf824b3416
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-26 20:09:19 -0300

    Inserindo o React Router

--- 15.Get_a_Pet/frontend/src/App.js
+++ 15.Get_a_Pet/frontend/src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+/* pages */
+import Home from './components/pages/Home'
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get a Pet</h1>      
-    </div>
+    <Router>
+        <Switch>
+            <Route path="/login">
+                <Login/>
+            </Route>
+            <Route path="/register">
+                <Register/>
+            </Route>
+            <Route path="/">
+                <Home/>
+            </Route>            
+        </Switch>
+    </Router>
   );
 }
 
