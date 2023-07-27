a872fe6a4e31444d582ee6baf6d9ae33a1c3fd56
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-26 21:07:25 -0300

    Criando Hook de Autenticação

---
+++ 15.Get_a_Pet/frontend/src/hooks/useAuth.js
@@ -0,0 +1,19 @@
+import api from '../utils/api'
+
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function useAuth() {
+    async function register(user) {   
+        try {
+          const data = await api.post('/users/register', user).then((response) => {
+                return response.data
+          })
+    
+          console.log(data)
+        } catch (error) {
+            // tratar erro
+            console.log(error)
+        }
+    }
+}
