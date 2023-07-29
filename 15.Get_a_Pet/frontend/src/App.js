826244d84791a836811d4445ecf71818eaa930d9
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-29 09:30:37 -0300

    Perfil de Usuário

--- 15.Get_a_Pet/frontend/src/App.js
+++ 15.Get_a_Pet/frontend/src/App.js
@@ -9,6 +9,7 @@ import Container from './components/layout/Container'
 import Home from './components/pages/Home'
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
+import Profile from './components/pages/User/Profile'
 
 /* contexts */
 import { UserProvider } from './context/UserContext'
@@ -26,6 +27,9 @@ function App() {
                     <Route path="/register">
                         <Register/>
                     </Route>
+                    <Route path="/user/profile">
+                        <Profile />
+                    </Route>                    
                     <Route path="/">
                         <Home/>
                     </Route>            
