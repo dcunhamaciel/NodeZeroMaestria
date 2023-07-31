701728643b9ebb167e0bc9ee25af5312cb198f60
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-31 20:35:44 -0300

    Fetch de um Post e seus Comentários

--- 19.Fetch_API/js/scripts.js
+++ 19.Fetch_API/js/scripts.js
@@ -3,6 +3,13 @@ const url = 'https://jsonplaceholder.typicode.com/posts'
 const loadingElement = document.querySelector('#loading')
 const postsContainer = document.querySelector('#posts-container')
 
+const postPage = document.querySelector('#post')
+const postContainer = document.querySelector('#post-container')
+const commentsContainer = document.querySelector('#comments-container')
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get('id')
+
 async function getAllPosts() {
     const response = await fetch(url)
 
@@ -19,7 +26,7 @@ async function getAllPosts() {
         title.innerText = post.title
         body.innerText = post.body
         link.innerText = 'Ler'
-        link.setAttribute('href', `/post.html?${post.id}`)
+        link.setAttribute('href', `/post.html?id=${post.id}`)
 
         div.appendChild(title)
         div.appendChild(body)
@@ -29,4 +36,48 @@ async function getAllPosts() {
     })
 }
 
-getAllPosts()
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ])
+
+    const dataPost = await responsePost.json()
+    const dataComments = await responseComments.json()
+
+    loadingElement.classList.add('hide')
+    postPage.classList.remove('hide')
+
+    const title = document.createElement('h1')
+    const body = document.createElement('p')
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+ 
+    postContainer.appendChild(title)
+    postContainer.appendChild(body)
+
+    dataComments.map((comment) => {
+        createComment(comment)
+    })
+}
+
+function createComment(comment) {
+    const div = document.createElement('div')
+    const email = document.createElement('h3')
+    const commentBody = document.createElement('p')
+
+    email.innerText = comment.email
+    commentBody.innerText = comment.body
+
+    div.appendChild(email)
+    div.appendChild(commentBody)
+
+    commentsContainer.appendChild(div)
+}
+
+if (!postId) {
+    getAllPosts()
+} else {
+    getPost(postId)
+}
